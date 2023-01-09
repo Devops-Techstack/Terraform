755676b452d830415f8ffc76336b921d50fa8e3c
Kritika-git <61340496+Kritika-git@users.noreply.github.com>
2023-01-10 08:36:13 +0900

    Code to Create Ec2 machine on awscloud

---
+++ ProvisionEC2Instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
